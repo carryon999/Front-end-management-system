7ace9c4fd458349ce395c35a90eb8e20f4f1bf6c
carry chen <carrychen999@outlook.com>
2023-07-28 17:32:05 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>我是home组件</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
