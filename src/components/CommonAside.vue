c791ec3e5e2243748e8bce0e4734ada95de388cf
carry chen <carrychen999@outlook.com>
2023-07-30 17:11:20 +0800

    3 commit

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,27 +1,27 @@
 <template>
 
-   <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+  <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
 
-  <el-menu-item v-for="item in noChildren()" :key="item.name" :index="item.name">
-      <i :class="`el-icon-${item.icon}`"></i>
-      <span slot="title">{{item.label}}</span>
-  </el-menu-item>
+    <el-menu-item v-for="item in noChildren" :key="item.name" :index="item.name">
+        <i :class="`el-icon-${item.icon}`"></i>
+        <span slot="title">{{item.label}}</span>
+    </el-menu-item>
 
-  <el-submenu index="1">
-    <template v-slot:title>
-      <i class="el-icon-location"></i>
-      <span slot="title">导航一</span>
-    </template>
-    <el-menu-item-group>
-      <!-- <span slot="title">分组一</span> -->
-      <el-menu-item index="1-1">选项1</el-menu-item>
+    <el-submenu v-for="item in hasChildren" :key="item.label" :index="item.label">
+      <template v-slot:title>
+        <i :class="`el-icon-${item.icon}`"></i>
+        <span slot="title">{{item.label}}</span>
+      </template>
 
-    </el-menu-item-group>
+      <el-menu-item-group v-for="subItem in item.children" :key="subItem.path">
+        <el-menu-item :index="subItem.path">{{subItem.labal}}</el-menu-item>
+      </el-menu-item-group>
 
-  </el-submenu>
-
+      <!-- <el-menu-item-group v-for="subItem in item.children" :key="subItem.path">
+        <el-menu-item :index="subItem.path">{{subItem.labal}}</el-menu-item>
+      </el-menu-item-group> -->
+    </el-submenu>
   </el-menu>
-
 </template>
 
 <script>
