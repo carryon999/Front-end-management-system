7e40b7022026a955acc33eb852de55ce032c94a2
carry chen <carrychen999@outlook.com>
2023-08-04 11:28:07 +0800

    user更改弹窗不起作用 commit

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <h1>我是user组件</h1>
+  <div class="manage">
+    <el-dialog
+      title="提示"
+      v-model:visible.sync="dialogVisible"
+      width="30%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+    <div class="manage-header">
+      <el-button type="primary" @click="dialogVisible = true">+ 新增</el-button>
+    </div>
   </div>
 </template>
 
